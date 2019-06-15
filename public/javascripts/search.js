1449c1adaa18a69f5f483e0c2fc1eb3de34ae337
hyemni <nhm77@naver.com>
2019-06-16 00:57:46 +0900

    final nav

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -8,3 +8,21 @@ $('.search-close, .overlay').click(function(){
   $('.search-drawer').removeClass('s-opened').addClass('s-closed');
   $('.overlay').toggle();
 });
+
+$(document).ready(function(){
+
+  //hides dropdown content
+  $(".size_chart").hide();
+
+  //unhides first option content
+  $("#option1").show();
+
+  //listen to dropdown for change
+  $("#size_select").change(function(){
+    //rehide content on change
+    $('.size_chart').hide();
+    //unhides current item
+    $('#'+$(this).val()).show();
+  });
+
+});
