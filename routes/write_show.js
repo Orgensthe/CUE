f35ad5b2c385fad15cb2d0e0af4d3509855a5ef1
orgensthe <w4102945@gmail.com>
2019-06-15 23:06:11 +0900

    test

--- routes/write_show.js
+++ routes/write_show.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('write_show.html');
 });
 
+router.post('/', function(req, res, next) {
+    res.render('write_show.html');
+  });
+
 module.exports = router;
