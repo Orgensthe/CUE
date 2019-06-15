75f2a7b9c13f56a232cc2c4b763a2fe243aa3b82
orgensthe <w4102945@gmail.com>
2019-06-16 02:47:37 +0900

    fix info_show.js

--- routes/info_show.js
+++ routes/info_show.js
@@ -7,7 +7,9 @@ router.get('/', function(req, res, next) {
   async function readPhost(){
     var dataValue = await fb.readPhost();
     console.log("ssdjjddjdj"+Buffer.from(dataValue.introduce, 'base64').toString('utf-8'));
+    console.log(Buffer.from(dataValue.fileURL, 'base64').toString('utf-8'))
     await res.render('info_show',{
+      fileUrl:Buffer.from(dataValue.fileURL, 'base64').toString('utf-8'),
       date:Buffer.from(dataValue.date, 'base64').toString('utf-8'),
       price:Buffer.from(dataValue.price, 'base64').toString('utf-8'),
       place:Buffer.from(dataValue.place, 'base64').toString('utf-8'),
