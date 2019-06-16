5e56e28d3274d6890454fff0deb23d8f920093ef
hyemni <nhm77@naver.com>
2019-06-17 01:14:26 +0900

    fuck off star

--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -1,5 +1,42 @@
-$('.starRev span').click(function(){
-  $(this).parent().children('span').removeClass('on');
-  $(this).addClass('on').prevAll('span').addClass('on');
-  return false;
-});
+// $('.starRev span').click(function(){
+//   $(this).parent().children('span').removeClass('on');
+//   $(this).addClass('on').prevAll('span').addClass('on');
+//   return false;
+// });
+
+var starRating = function(){
+  var $star = $(".star-input"),
+    $result = $star.find("output>b");
+
+  $(document)
+    .on("focusin", ".star-input>.input",
+      function(){
+        $(this).addClass("focus");
+      })
+
+    .on("focusout", ".star-input>.input", function(){
+      var $this = $(this);
+      setTimeout(function(){
+        if($this.find(":focus").length === 0){
+          $this.removeClass("focus");
+        }
+      }, 100);
+    })
+
+    .on("change", ".star-input :radio", function(){
+      $result.text($(this).next().text());
+    })
+    .on("mouseover", ".star-input label", function(){
+      $result.text($(this).text());
+    })
+    .on("mouseleave", ".star-input>.input", function(){
+      var $checked = $star.find(":checked");
+      if($checked.length === 0){
+        $result.text("0");
+      } else {
+        $result.text($checked.next().text());
+      }
+    });
+};
+
+starRating();
